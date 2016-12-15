621f6af36a0bad6779396ac0673a6a7e9caa237b
zhu_w <15828513763@163.com>
2016-12-15 23:12:31 +0800

    第9课

--- n9_exception.js
+++ n9_exception.js
@@ -2,19 +2,21 @@
  * Created by Administrator on 2016/12/4.
  */
 //-------------n9_exception.js---------------
+
+// 通过对异常的抛出来解决很多地方的判断!!!
 var http = require('http');
 var url = require('url');
 var router = require('./router');
-//var exception = require('./models/Exception');
+var exception = require('./models/Exception');
 http.createServer(function (request, response) {
     if (request.url !== "/favicon.ico") { //清除第2此访问
         var pathname = url.parse(request.url).pathname;
         pathname = pathname.replace(/\//, '');//替换掉前面的/
         try {
-            router[pathname](request,response);
-            //data = exception.expfun(0);
-            //response.write(data);
-            //response.end('');
+            //router[pathname](request,response);
+            data = exception.expfun(0);
+            response.write(data);
+            response.end('');
         } catch (err) {
             console.log('错误: ' + err);
             response.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
--- router.js
+++ router.js
@@ -3,8 +3,38 @@
 
 //路由读取文件,添加需要打开的页面或者读取的文件方法
 var openfile = require('./models/optfile');
+var url  =  require('url');
+var querystring  =  require('querystring');  //post需导入
 
 module.exports = {
+    //提交
+    submit: function(req,res){
+        //---------GET方式--------
+        /*
+         var rdata = url.parse(req.url,true).query;
+         console.log('GET收到的参数:',rdata);
+         */
+        //-----------POST方式------------
+        var post = '';          //定义了一个post变量，用于暂存请求体的信息
+        req.on('data', function (chunk) {        //通过req的data事件监听函数，每当接受到请求体的数据，就累加到post变量中
+            post += chunk;
+        });
+        //-------注意异步-------------
+        req.on('end', function () {        //在end事件触发后，通过querystring.parse将post解析为真正的POST请求格式，然后向客户端返回。
+            post = querystring.parse(post);
+            setTimeout(function(){
+                console.log('POST收到的参数:',post);
+                //显示页面
+                if(post.user){
+                    openfile.readfile('./views/result1.html',getRecall(req,res));
+                }else{
+                    res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
+                    res.write('请填写表单元素');
+                    res.end('');
+                }
+            },1000);
+        });
+    },
     //登录
     login: function(req,res){
         openfile.readfile('./views/login.html',getRecall(req,res));
@@ -34,10 +64,11 @@ module.exports = {
 
 //操作文件的方式
 function getRecall(req,res){
-    //res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
+    res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
     function recall(data){
         res.write(data);
         res.end('');
     }
     return recall;
 }
+
