0ec7f9a1cc669e995ebcff5ae78338a1dbb2f827
zhu_w <15828513763@163.com>
2017-06-06 21:40:28 +0800

    nodejs 基础

---
+++ zns/node3/fs.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+//fs.readFile();
+//fs.writeFile()
+//readFile(文件名, 回调函数)
+fs.readFile('aaa.txt', function (err, data){
+  if(err){
+    console.log('读取失败');
+  }else{
+    console.log(data.toString());
+  }
+});
+
+//fs.writeFile
