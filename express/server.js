2d4169a33dcca0145f9ae4cfe468ea589b3b96b1
zhu_w <15828513763@163.com>
2018-02-08 12:54:48 +0800

    express 调试

--- express/server.js
+++ express/server.js
@@ -1,34 +1,35 @@
-const express=require('express');
-const static=require('express-static');
-const bodyParser=require('body-parser');
-const multer=require('multer');
-const multerObj=multer({dest: './static/upload'});
-const mysql=require('mysql');
-const cookieParser=require('cookie-parser');
-const cookieSession=require('cookie-session');
-const consolidate=require('consolidate');
-const expressRoute=require('express-route');
+const express = require('express');
+const static = require('express-static');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const multerObj = multer({dest: './static/upload'});
+const mysql = require('mysql');
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
+const consolidate = require('consolidate');
+const expressRoute = require('express-route');
+const server = express();
+server.listen(8080,function () {
+    console.log('server running port:8080');
+});
 
-var server=express();
-server.listen(8080);
 
 //1.获取请求数据
 //get自带
-server.use(bodyParser.urlencoded());
+server.use(bodyParser.urlencoded({extended:false}));
 server.use(multerObj.any());
-
 //2.cookie、session
 server.use(cookieParser());
-(function (){
-  var keys=[];
-  for(var i=0;i<100000;i++){
-    keys[i]='a_'+Math.random();
-  }
-  server.use(cookieSession({
-    name: 'sess_id',
-    keys: keys,
-    maxAge: 20*60*1000  //20min
-  }));
+(function () {
+    let keys = [];
+    for (let i = 0; i < 100000; i++) {
+        keys[i] = 'a_' + Math.random();
+    }
+    server.use(cookieSession({
+        name: 'sess_id',
+        keys: keys,
+        maxAge: 20 * 60 * 1000  //20min
+    }));
 })();
 
 //3.模板
